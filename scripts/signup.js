56af45a4839bb8d08cca49a26f649c008999d96e
Cisija <115004298+Cisija@users.noreply.github.com>
2023-04-25 11:43:07 +0200

    Prvi commit

---
+++ scripts/signup.js
@@ -0,0 +1,7 @@
+document.querySelector("#driver").addEventListener("click", () => {
+  window.open("signupdriver.html", "_self");
+});
+
+document.querySelector("#rider").addEventListener("click", () => {
+  window.open("signuprider.html", "_self");
+});
